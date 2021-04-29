c8f9995ec32f6fdd5ec9159f925e88cd8a3c5ff8
Devansh Chaubey <devkumar5436@gmail.com>
2021-04-30 04:22:22 +0530

    completed functionalities

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Modals from './Modals';
 import axios from 'axios';
+import Loader from '../assets/loader.svg'
+import InfiniteScroll from "react-infinite-scroll-component";
 export default function Images() {
     const [photos, setPhotos] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [modalphoto, setModalphoto] = useState({});
     const [search, setSearch] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [next, setNext] = useState(1);
+    const [hasMore, sethasMore] = useState(true);
     const openModal = (photo) => {
         setModalphoto(photo);
         setShowModal(true);
@@ -19,38 +22,103 @@ export default function Images() {
 
     useEffect(() => {
         if (!search) {
-            axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getrecent&api_key=${process.env.REACT_APP_API_KEY}&format=json&nojsoncallback=1`)
-                .then(res => {
-                    setPhotos(res.data.photos.photo)
-                })
+            getRecent();
         } else {
             getAllData();
         }
 
-    }, [search]);
+    }, [search, next]);
 
     const getAllData = () => {
         axios
-            .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&format=json&nojsoncallback=1&text=${search}`)
+            .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&format=json&nojsoncallback=1&text=${search}&page=${next}&per_page=9`)
             .then((res) => {
-                console.log(res)
-                setPhotos(res.data.photos.photo);
+                setTimeout(() => {
+                    setPhotos(photos.concat(res.data.photos.photo));
+                }, 1000)
             });
     };
 
+    const getRecent = ()=>{
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getrecent&api_key=${process.env.REACT_APP_API_KEY}&format=json&nojsoncallback=1&page=${next}&per_page=9`)
+                .then(res => {
+                    setTimeout(() => {
+                        setPhotos(photos.concat(res.data.photos.photo))
+                    }, 1000)
+                })
+    }
+
     const handleChange = (event) => {
-        console.log('working')
+        setNext(1);
+        setPhotos([])
         setSearch(event.target.value);
     };
-    return (
-        <>
-            <div className="bg-gray-100 w-full h-screen py-10">
 
-                <p className="font-serif text-6xl text-center mb-5">Search Photos</p>
-                <div className="sticky top-10 bg-white flex  mx-10 md:mx-96 shadow-xl">
-                    <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" onChange={handleChange} />
-                </div>
-                {loading ? <Loader/> : (
+    const fetchMoreData = () => {
+        if (photos.length >= 1000) {
+            sethasMore(false);
+        } else {
+            setNext(next + 1);
+        }
+    }
+
+    const handleSearchSuggestion = () => {
+        if(localStorage.getItem("suggestions")===null && search){
+            localStorage.setItem("suggestions",search.toString());
+        }else if(search){
+            let suggestion=localStorage.getItem("suggestions").split(" ")
+            suggestion.push(search);
+            localStorage.setItem("suggestions",suggestion.toString());
+        }
+
+    }
+
+    const handleFocus = () =>{
+        if(!search){
+            setPhotos([])
+            getRecent();
+        }
+    }
+
+    return (
+        <div className="bg-gray-100">
+            <p className="font-serif text-6xl text-center pt-5 mb-5">Search Photos</p>
+            <div>
+
+            </div>
+            <div className="sticky top-40 bg-white flex flex-col  mx-10 md:mx-96 shadow-xl">
+                <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" onChange={handleChange} onFocus={handleFocus} onBlur={handleSearchSuggestion} />
+                <div className="divide-y divide-light-blue-400" >
+                    {
+                        localStorage.getItem("suggestions")?.split(",").slice(0,5).map((item,index)=>{
+                            return <div className="pl-3" key={index}>{item}</div>
+                        })
+                    }
+                </div>    
+            </div>
+            
+            <InfiniteScroll
+                dataLength={photos.length}
+                next={fetchMoreData}
+                hasMore={hasMore}
+                loader={
+                    <div className="flex justify-center">
+                        <img src={Loader} className="animate-spin" />
+                    </div>
+                }
+                endMessage={
+                    <p style={{ textAlign: "center" }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                }
+                style={{ overflow: "hidden" }}
+            >
+                <div className="bg-gray-100 py-10">
+
+
+
+
+
                     <div className="container mx-auto pt-20">
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 place-items-center">
                             {photos.map((photo, index) => {
@@ -61,13 +129,12 @@ export default function Images() {
                                 )
                             })}
                         </div>
-
                     </div>
+                    <Modals modalphoto={modalphoto} showModal={showModal} closeModal={closeModal} />
+                </div>
 
-                )}
+            </InfiniteScroll>
 
-            </div>
-            <Modals modalphoto={modalphoto} showModal={showModal} closeModal={closeModal} />
-        </>
+        </div>
     )
 }
